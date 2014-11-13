6ded65ae1db3278a67a7cb82c3bb8e113753ab56
Rémy Sanchez <remy.sanchez@hyperthese.net>
2014-11-13 16:28:14 +0100

    Setupable package

---
+++ setup.py
@@ -0,0 +1,49 @@
+# vim: fileencoding=utf-8 tw=100 expandtab ts=4 sw=4 :
+#
+# pylesswrap
+# (c) 2014 Rémy Sanchez <remy.sanchez@activkonnect.com>
+#
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING file for more details.
+
+import os
+import codecs
+from setuptools import setup
+
+with codecs.open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='pylesswrap',
+    version='0.1',
+    packages=['pylesswrap'],
+    include_package_data=True,
+    license='WTFPL',
+    description='A Python wrapper around LESS.js, destined to help compiling LESS files from '
+                'Django.',
+    long_description=README,
+    url='https://github.com/Xowap/pylesswrap',
+    author='Rémy Sanchez',
+    author_email='remy.sanchez@activkonnect.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Licence :: Other/Proprietary License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
