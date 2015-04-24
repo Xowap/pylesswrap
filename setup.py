3f5df61391525d8b8222b482931930cad5ea44ff
Rémy Sanchez <remy.sanchez@hyperthese.net>
2015-04-24 10:49:41 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='pylesswrap',
-    version='0.1.3',
+    version='0.1.4',
     packages=['pylesswrap'],
     include_package_data=True,
     license='WTFPL',
